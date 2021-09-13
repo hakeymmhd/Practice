7d64ea530841ce2d2b9a4cb2b4729146c2ad854c
mhdhakeym <mhdhakeym786@gmail.com>
2021-09-13 23:29:37 +0800

    all intended features running

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ console.log("test links");
 
 const toDoList = [];
 
+const removeItem = () => {
+    $('#clearList').click((e) => {
+        $('.completedList').empty();
+    })
+}
+
 const pushDone = () => {
     $('.done').click((e) => {        // why doesn't this work in Document.ready fn?
-        console.log('the event type is: ' + e.type);
-        console.log("triggertarget is: " + e.target);
-        console.log("current target is: " + e.currentTarget);
+        // console.log('the event type is: ' + e.type);
+        // console.log("triggertarget is: " + e.target);
+        // console.log("current target is: " + e.currentTarget);
         const deletedIndex = $(e.target).parent().index();
-        // e.stopPropagation();
-        $(e.target).parent().appendTo('#completed');
-        toDoList.splice(deletedIndex, 1);
-        // console.log($(e.target)); 
-        // const findList = $('#tdl')
-        
-        // console.log(str);
-        // console.log(toDoList.indexOf(str));
-        // render();
+        $(e.target).parent().appendTo('.completedList');
+        toDoList.splice(deletedIndex, 1); //updates array
+        $(e.target).attr('class', 'remove').text('Remove'); //changes the class of button but listener sees it as old one
+        $(e.target).remove();
     })
     
 }
@@ -30,26 +31,20 @@ const render = (str, e) => {
         $newOL.append($newItem);
         const $newButton = $('<button type="button" class="done">Completed!</button>');
         $newItem.append($newButton);
-        $('#tdl').append($newOL);
-        // $('<button type="button" class="test">test</button>').appendTo($newDiv);
+           
     }) 
+    $('#tdl').append($newOL);
     pushDone();
 }
 
 $(() => {
     $('#submit').click((e) => {
         let str = $('#input-box').val();
-        toDoList.push(str);
+        toDoList.push(str); //updates array
        render(str, e);
        $('#input-box').val('');
     }) 
 
-    // $('.hello').click((e) => {
-    //     console.log('the event type is: ' + e.type);
-    //     console.log("triggertarget is: " + e.target);
-    //     console.log("current target is: " + e.currentTarget);
-    //     // e.stopPropagation();
-    //     $(e.target).appendTo('#completed'); 
-    // })
-
+    removeItem();
+    
 })
